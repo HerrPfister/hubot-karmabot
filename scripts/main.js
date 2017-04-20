db0845b49d4706179022a78c9530450a1ce30d9f
Matthew Pfister <herrpfister@gmail.com>
2017-04-20 14:29:30 -0500

    Fixing bugs.

--- scripts/main.js
+++ scripts/main.js
@@ -8,25 +8,38 @@
 // Author:
 //   HerrPfister
 
+function sendKarmaMessage(robot, res, karmaAmount) {
+    var subject = res.match[1] || res.match[2];
+
+    var oldKarmaAmount = parseInt(robot.brain.get(subject)) || 0;
+    var newKarmaAmount = oldKarmaAmount + karmaAmount;
+
+    var karmaText = "not changed";
+
+    robot.brain.set(subject, newKarmaAmount);
+
+    if (oldKarmaAmount < newKarmaAmount) {
+        karmaText = "increased to {amount}".replace("{amount}", newKarmaAmount.toString());
+    } else if (oldKarmaAmount > newKarmaAmount) {
+        karmaText = "decreased to {amount}".replace("{amount}", newKarmaAmount.toString());
+    }
+
+    res.send("{subject}\'s karma has {karmaText}."
+        .replace("{subject}", subject)
+        .replace("{karmaText}", karmaText));
+}
+
 module.exports = function (robot) {
-    robot.hear(/s*"(.*)"[+-]+|\s*(@\w+)\s?[+-]+/i, function (res) {
-        var subject = res.match[1] || res.match[2];
-        var positiveKarmaAmount = (res.message.text.match(/[+]/g) || []).length;
-        var negativeKarmaAmount = (res.message.text.match(/[-]/g) || []).length;
-        var oldKarmaAmount = parseInt(res.brain.get(subject));
-        var newKarmaAmount = oldKarmaAmount + positiveKarmaAmount - negativeKarmaAmount;
-        var karmaText = "not changed";
+    robot.hear(/s*"(.*)"[+]{2}[+]*|\s*(@\w+)\s?[+]{2}[+]*/i, function (res) {
+        var plusCount = (res.message.text.match(/[+]/g) || []).length;
+        var positiveKarmaAmount = plusCount > 2 ? plusCount : 1;
 
-        res.brain.set(subject, newKarmaAmount);
+        sendKarmaMessage(robot, res, positiveKarmaAmount);
+    });
+    robot.hear(/s*"(.*)"[-]{2}[-]*|\s*(@\w+)\s?[-]{2}[-]*/i, function (res) {
+        var minusCount = (res.message.text.match(/[-]/g) || []).length;
+        var negativeKarmaAmount = -(minusCount > 2 ? minusCount : 1);
 
-        if (oldKarmaAmount < newKarmaAmount) {
-            karmaText = "increased to {amount}".replace("{amount}", newKarmaAmount.toString());
-        } else if (oldKarmaAmount > newKarmaAmount) {
-            karmaText = "decreased to {amount}".replace("{amount}", newKarmaAmount.toString());
-        }
-
-        res.send("{subject}\'s karma has {karmaText}.")
-            .replace("{subject}", subject)
-            .replace("{karmaText}", karmaText);
+        sendKarmaMessage(robot, res, negativeKarmaAmount);
     });
 };

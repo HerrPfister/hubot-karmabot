c602ee3d5019632020deebdd8f27964d95a853d3
Matthew Pfister <herrpfister@gmail.com>
2017-04-20 13:42:30 -0500

    Allowing for message sent to change based on new karma value.

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 //   A simple hubot that keeps track of karma.
 //
 // Commands:
-//   hubot @[user]++
+//   hubot @[subject]++
 //   hubot "[subject]"++
 //
 // Author:
@@ -13,12 +13,20 @@ module.exports = function (robot) {
         var subject = res.match[1] || res.match[2];
         var positiveKarmaAmount = (res.message.text.match(/[+]/g) || []).length;
         var negativeKarmaAmount = (res.message.text.match(/[-]/g) || []).length;
-        var newKarmaAmount = parseInt(res.brain.get(subject)) + positiveKarmaAmount - negativeKarmaAmount;
+        var oldKarmaAmount = parseInt(res.brain.get(subject));
+        var newKarmaAmount = oldKarmaAmount + positiveKarmaAmount - negativeKarmaAmount;
+        var karmaText = "not changed";
 
         res.brain.set(subject, newKarmaAmount);
 
-        res.send("{subject}\'s karma has changed to {karmaAmount}"
+        if (oldKarmaAmount < newKarmaAmount) {
+            karmaText = "increased to {amount}".replace("{amount}", newKarmaAmount.toString());
+        } else if (oldKarmaAmount > newKarmaAmount) {
+            karmaText = "decreased to {amount}".replace("{amount}", newKarmaAmount.toString());
+        }
+
+        res.send("{subject}\'s karma has {karmaText}.")
             .replace("{subject}", subject)
-            .replace("{karmaAmount}", newKarmaAmount.toString()));
+            .replace("{karmaText}", karmaText);
     });
 };

e965fddf398f9262f4bc6aebc57d6f94020c0280
Matthew Pfister <herrpfister@gmail.com>
2017-04-24 10:01:20 -0500

    Adding karma list command

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,8 @@
 //   A simple hubot that keeps track of karma.
 //
 // Commands:
+//   hubot karma list
 //   @[subject]++
-//   "[subject]"++
 //
 // Author:
 //   HerrPfister
@@ -11,12 +11,19 @@
 function sendKarmaMessage(robot, res, karmaAmount) {
     var subject = res.match[1] || res.match[2];
 
+    var karmaKeys = robot.brain.get('keys') || [];
+
     var oldKarmaAmount = parseInt(robot.brain.get(subject)) || 0;
     var newKarmaAmount = oldKarmaAmount + karmaAmount;
 
     var karmaText = "not changed";
 
     robot.brain.set(subject, newKarmaAmount);
+
+    if (karmaKeys.indexOf(subject) < 0) {
+        karmaKeys.push(subject);
+        robot.brain.set('keys', karmaKeys);
+    }
 
     if (oldKarmaAmount < newKarmaAmount) {
         karmaText = "increased to {amount}".replace("{amount}", newKarmaAmount.toString());
@@ -30,12 +37,25 @@ function sendKarmaMessage(robot, res, karmaAmount) {
 }
 
 module.exports = function (robot) {
+    robot.respond(/karma list/i, function (res) {
+        var karmaKeys = robot.brain.get('keys');
+
+        var messages = karmaKeys.map(function (key) {
+            var val = robot.brain.get(key);
+
+            return "{key} = {value}".replace("{key}", key).replace("{value}", val);
+        });
+
+        res.send(messages.join("\n"));
+    });
+
     robot.hear(/\s*(@\w+)\s?[+]{2}[+]*/i, function (res) {
         var plusCount = (res.message.text.match(/[+]/g) || []).length;
         var positiveKarmaAmount = plusCount > 2 ? plusCount - 1 : 1;
 
         sendKarmaMessage(robot, res, positiveKarmaAmount);
     });
+
     robot.hear(/\s*(@\w+)\s?[-]{2}[-]*/i, function (res) {
         var minusCount = (res.message.text.match(/[-]/g) || []).length;
         var negativeKarmaAmount = -(minusCount > 2 ? minusCount - 1 : 1);
